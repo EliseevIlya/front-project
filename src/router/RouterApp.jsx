008f00c5a51fcd0d9b16b19e141d6f0b2c264c1c
akiyanaraa <166604725+akiyanaraa@users.noreply.github.com>
2025-03-01 17:18:26 +0400

    login+createaccount page
    Создание loginpage и createaccpage страниц

--- src/router/RouterApp.jsx
+++ src/router/RouterApp.jsx
@@ -1,13 +1,15 @@
 import { Route, Routes } from "react-router";
-
+import Createaccpage from "../pages/createaccpage/createaccpage.jsx"
 import Loginpage from "../pages/loginpage/loginpage.jsx";
+
 
 
 export default function RouterApp(){
 
     return(
             <Routes>
-                <Route path="/" element={<Loginpage />}/>
+                <Route path="/loginpage" element={<Loginpage />}/>
+                <Route path="/createaccpage" element={<Createaccpage />}/>
             </Routes>
     )
 }
