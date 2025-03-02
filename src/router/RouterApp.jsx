e1bc86fc0363849c8ec0cc5035b8bb2824c0953a
Bys0kbata <92207641+bys0kbata@users.noreply.github.com>
2025-03-02 17:47:12 +0300

    Update RouterApp.jsx

--- src/router/RouterApp.jsx
+++ src/router/RouterApp.jsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router";
 import Createaccpage from "../pages/createaccpage/createaccpage.jsx"
 import Loginpage from "../pages/loginpage/loginpage.jsx";
-
+import Main_page from "../pages/main_page/main_page.jsx";
 
 
 export default function RouterApp(){
@@ -10,6 +10,7 @@ export default function RouterApp(){
             <Routes>
                 <Route path="/loginpage" element={<Loginpage />}/>
                 <Route path="/createaccpage" element={<Createaccpage />}/>
+                <Route path="/" element={<Main_page />}/>
             </Routes>
     )
 }
