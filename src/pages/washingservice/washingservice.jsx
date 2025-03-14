86a51fe35432de956c09bfac04dc7667e3def05f
Bys0kbata <92207641+bys0kbata@users.noreply.github.com>
2025-03-15 01:04:20 +0400

    Update washingservice.jsx

--- src/pages/washingservice/washingservice.jsx
+++ src/pages/washingservice/washingservice.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import "./style.css";
-import { useState } from "react";
 import Modal from 'react-modal'
 import Confirmationwashingpage from "../confirmationwashingpage/confirmationwashingpage";
 
