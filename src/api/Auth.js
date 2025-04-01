07313571f08cec2b5de41351fe92657cf1bcb031
akiyanaraa <166604725+akiyanaraa@users.noreply.github.com>
2025-04-01 23:55:53 +0400

    send bodys api
    body send back

--- src/api/Auth.js
+++ src/api/Auth.js
@@ -1,17 +1,22 @@
 import axios from "axios";
 
-const api = "http://217.107.34.217:9919";
+const api = "http://localhost:8080"
 
-export function registercustomer(){
+export function registercustomer(surnameregistercustomer,nameregistercustomer, patronymicregistercustomer, phoneNumberregistercustomer, addInforegistercustomer, emailregistercustomer){
     const body={
-
-    }
+    surname:surnameregistercustomer,
+    name:nameregistercustomer,
+    patronymic:patronymicregistercustomer,
+    phoneNumber:phoneNumberregistercustomer,
+    addInfo:addInforegistercustomer,
+    email:emailregistercustomer
+}
     const headers={
 
     }
     axios.post(`${api}/auth/sign_up/customer`,body,headers)
-    .then(()=>{
-
+    .then((res)=>{
+        console.log(res)
     })
     .catch((error)=>{
         console.error('Ошибка при отправке запроса:', error.response ? error.response.data : error.message);
@@ -22,9 +27,30 @@ export function registercustomer(){
         }
     })
 }
-export function registerorg(){
-    const body={
 
+export function registerorg(fullNameregisterorg, shortNameregisterorg, innregisterorg, kppregisterorg, ogrnregisterorg, responsiblePersonSurnameregisterorg, responsiblePersonNameregisterorg, responsiblePersonPatronymicregisterorg, responsiblePersonEmailregisterorg, responsiblePersonPhoneNumberregisterorg, addInforegisterorg,
+    subjectNameregisterorg, cityNameregisterorg, streetNameregisterorg, houseNumberregisterorg, addInfoadressregisterorg, addressTyperegisterorg, emailregisterorg){
+    const body={
+    fullName:fullNameregisterorg,
+    shortName:shortNameregisterorg,
+    inn:innregisterorg,
+    kpp:kppregisterorg,
+    ogrn:ogrnregisterorg,
+    responsiblePersonSurname:responsiblePersonSurnameregisterorg,
+    responsiblePersonName:responsiblePersonNameregisterorg,
+    responsiblePersonPatronymic:responsiblePersonPatronymicregisterorg,
+    responsiblePersonEmail:responsiblePersonEmailregisterorg,
+    responsiblePersonPhoneNumber:responsiblePersonPhoneNumberregisterorg,
+    addInfo:addInforegisterorg,
+    address: {
+    subjectName:subjectNameregisterorg,
+    cityName:cityNameregisterorg,
+    streetName:streetNameregisterorg,
+    houseNumber:houseNumberregisterorg,
+    addInfo:addInfoadressregisterorg,
+    addressType:addressTyperegisterorg
+    },
+    email:emailregisterorg
     }
     const headers={
 
@@ -42,9 +68,19 @@ export function registerorg(){
         }
     })
 }
-export function registeradmin(){
-    const body={
 
+export function registeradmin(surnameregisteradmin, nameregisteradmin, patronymicregisteradmin, departmentregisteradmin, positionregisteradmin, phoneNumberregisteradmin, 
+    addInforegisteradmin, emailregisteradmin, passwordregisteradmin){
+    const body={
+    surname:surnameregisteradmin,
+    name:nameregisteradmin,
+    patronymic:patronymicregisteradmin,
+    department:departmentregisteradmin,
+    position:positionregisteradmin,
+    phoneNumber:phoneNumberregisteradmin,
+    addInfo:addInforegisteradmin,
+    email:emailregisteradmin,
+    password:passwordregisteradmin
     }
     const headers={
 
@@ -62,23 +98,23 @@ export function registeradmin(){
         }       
     })
 }
+
 export function sendcode(email) {
     const headers ={
         "Content-Type": "application/json",
-        'Access-Control-Allow-Credentials':true
     }
-    console.log(headers)
     return axios.post(`${api}/auth/sign_in/send_code?email=${email}`,
-        {}, // Пустой объект в body, если сервер требует тело запроса
+        {}, 
         {
             headers: {
-                "Content-Type": "application/json"
+                "Content-Type": "application/json",
             },
-            withCredentials: true // Если сервер требует куки
         })
         .then((res) => {
+            console.log(res)
             if (res.status === 200) {
                 console.log('Код успешно отправлен');
+                console.log(res);
                 return true; // Успешная отправка
             } else {
                 console.error(`Ошибка: сервер вернул статус ${res.status}`);
@@ -96,19 +132,21 @@ export function sendcode(email) {
             return false; // Ошибка запроса
         });
 }
-
-
-
-export function authcustomer(){
+export function authcustomer(emailauthcustomer, codeauthcustomer){
     const body={
-
-    }
+    email:emailauthcustomer,
+    code:codeauthcustomer
+}
+console.log(body)
     const headers={
 
     }
     axios.post(`${api}/auth/sign_in/customer`,body,headers)
-    .then(()=>{
-
+    .then((response)=>{
+        const token = response.data; // или response.data.accessToken, зависит от сервера
+        localStorage.setItem('jwtToken', token);
+        console.log(data);
+        return response.data; // Возвращаем данные для дальнейшей обработки
     })
     .catch(()=>{
         console.error('Ошибка при отправке запроса:', error.response ? error.response.data : error.message);
@@ -120,9 +158,10 @@ export function authcustomer(){
     })
 }
 
-export function authorg(){
+export function authorg(emailauthorg, codeauthorg){
     const body={
-
+    email:emailauthorg,
+    code:codeauthorg
     }
     const headers={
 
@@ -141,9 +180,11 @@ export function authorg(){
     })
 }
 
-export function authadmin(){
+export function authadmin(emailauthadmin, passwordauthadmin, codeauthadmin){
     const body={
-
+    email:emailauthadmin,
+    password:passwordauthadmin,
+    code:codeauthadmin
     }
     const headers={
 
